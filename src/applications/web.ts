f475aabb82dfe55c7c8975e09672fefceabe83af
Adi Wahyudi <adi.whyd02@gmail.com>
2025-07-23 17:47:04 +0700

    feat: add error handling for ZodError and ResponseError type

--- src/applications/web.ts
+++ src/applications/web.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { errorMiddleware } from "../middlewares/error.middleware";
 
 export const web = express();
 web.use(express.json());
+web.use(errorMiddleware);
