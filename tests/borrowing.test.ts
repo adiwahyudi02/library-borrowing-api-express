0e62f24bfdca0ffc5ddb39a302e84cd9fe7bd514
Adi Wahyudi <adi.whyd02@gmail.com>
2025-07-25 12:44:21 +0700

    test: add test cases for the get all borrowings api

--- tests/borrowing.test.ts
+++ tests/borrowing.test.ts
@@ -2,6 +2,7 @@ import supertest from "supertest";
 import { TestUtils } from "./utils";
 import { web } from "../src/applications/web";
 import { logger } from "../src/applications/logger";
+import { Borrowing } from "@prisma/client";
 
 describe("Borrowing", () => {
   describe("POST /api/borrowings", () => {
@@ -129,6 +130,126 @@ describe("Borrowing", () => {
 
       expect(fail.status).toBe(400);
       expect(fail.body.errors).toBeDefined();
+    });
+  });
+
+  describe("GET /api/borrowings", () => {
+    beforeEach(async () => {
+      await TestUtils.CreateDummyGuard();
+      await TestUtils.CreateDummyMember();
+      await TestUtils.CreateDummyBook();
+      await TestUtils.CreateDummyBorrowing();
+    });
+
+    afterEach(async () => {
+      await TestUtils.DeleteDummyBorrowing();
+      await TestUtils.DeleteDummyMember();
+      await TestUtils.DeleteDummyBook();
+      await TestUtils.DeleteDummyGuard();
+    });
+
+    it("should return all borrowings with pagging", async () => {
+      const response = await supertest(web)
+        .get("/api/borrowings")
+        .set("X-API-TOKEN", "test");
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeDefined();
+      expect(response.body.pagging).toBeDefined();
+    });
+
+    it("should return the borrowings based on the name of the member", async () => {
+      const member = await TestUtils.GetDummyMember();
+      const response = await supertest(web)
+        .get("/api/borrowings")
+        .query({
+          search: member.name,
+        })
+        .set("X-API-TOKEN", "test");
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeDefined();
+      expect(response.body.data.length).toBeGreaterThan(0);
+      const firstItem = response.body.data[0];
+      expect(firstItem.member.name).toContain(member.name);
+    });
+
+    it("should return the borrowings based on the nenber email", async () => {
+      const member = await TestUtils.GetDummyMember();
+
+      const response = await supertest(web)
+        .get("/api/borrowings")
+        .query({
+          search: member.email,
+        })
+        .set("X-API-TOKEN", "test");
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeDefined();
+
+      expect(response.body.data.length).toBeGreaterThan(0);
+      const firstItem = response.body.data[0];
+      expect(firstItem.member.email).toContain(member.email);
+    });
+
+    it("should return the borrowings based on the title books", async () => {
+      const book = await TestUtils.GetDummyBook();
+
+      const response = await supertest(web)
+        .get("/api/borrowings")
+        .query({
+          search: book.title,
+        })
+        .set("X-API-TOKEN", "test");
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeDefined();
+
+      expect(response.body.data.length).toBeGreaterThan(0);
+      const firstItem = response.body.data[0];
+      expect(firstItem.book.title).toContain(book.title);
+    });
+
+    it("should return the borrowings based on the status", async () => {
+      const response = await supertest(web)
+        .get("/api/borrowings")
+        .query({
+          status: "borrowed",
+        })
+        .set("X-API-TOKEN", "test");
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeDefined();
+
+      const check = response.body.data.filter(
+        (item: Borrowing) => item.returnDate === null
+      );
+
+      expect(check.length).toBeGreaterThan(0);
+    });
+
+    it("should return right page and size on the pagging", async () => {
+      const response = await supertest(web)
+        .get("/api/borrowings")
+        .query({
+          page: 2,
+          size: 5,
+        })
+        .set("X-API-TOKEN", "test");
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeDefined();
+      expect(response.body.pagging.current_page).toBe(2);
+      expect(response.body.pagging.size_page).toBe(5);
+    });
+
+    it("should failed 401 to get borrowings if the token is wrong", async () => {
+      const response = await supertest(web)
+        .get("/api/borrowings")
+        .set("X-API-TOKEN", "wrong");
+
+      expect(response.status).toBe(401);
+      expect(response.body.errors).toBeDefined();
     });
   });
 });
